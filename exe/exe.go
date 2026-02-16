3f1ac614837ef18ee6228ff57258ef5c7184a47d
zichou <1303037989@qq.com>
2026-02-16 12:28:35 +0800

    arg ...string

--- exe/exe.go
+++ exe/exe.go
@@ -6,16 +6,19 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/zichouu/go-pkg/color"
 )
 
-func Run(dir string, command string, aenv []string) error {
+func Run(dir string, aenv []string, command ...string) error {
 	var cmd *exec.Cmd
 	if runtime.GOOS == "windows" {
-		cmd = exec.Command("cmd", "/c", command)
+		arg := append([]string{"/c"}, command...)
+		cmd = exec.Command("cmd", arg...)
 	} else {
-		cmd = exec.Command("sh", "-c", command)
+		arg := append([]string{"-c"}, command...)
+		cmd = exec.Command("sh", arg...)
 	}
 	if len(aenv) > 0 {
 		env := os.Environ()
@@ -26,12 +29,12 @@ func Run(dir string, command string, aenv []string) error {
 	}
 	cmd.Dir = dir
 	errColor := color.BgGreen
-	fmt.Println(color.BgBlue, "执行", dir, command, color.Reset)
+	fmt.Println(color.BgBlue, "执行", dir, strings.Join(command, " "), color.Reset)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		errColor = color.BgRed
 	}
-	fmt.Println(errColor, "完成", dir, command, color.Reset)
+	fmt.Println(errColor, "完成", dir, strings.Join(command, " "), color.Reset)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -39,11 +42,11 @@ func Run(dir string, command string, aenv []string) error {
 	return err
 }
 
-func IfExist(path string, filename string, command string, aenv []string) error {
+func IfExist(path string, filename string, aenv []string, command ...string) error {
 	join := filepath.Join(path, filename)
 	_, err := os.Stat(join)
 	if err == nil {
-		err := Run(path, command, aenv)
+		err := Run(path, aenv, command...)
 		return err
 	}
 	return nil
